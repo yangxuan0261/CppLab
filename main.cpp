d2d57da0f29abb43fcfa671588de6061d0e62ac8
wilker <364105996@qq.com>
2020-02-09 19:24:11 +0800

    - add lib

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "test.h"
 
+#include "library.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     CTest::print();
+    hello();
     system("pause");
     return 0;
 }
