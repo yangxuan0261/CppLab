f8c382a6e54edf031d002a41d2a72813bb33fe21
wilker <364105996@qq.com>
2020-02-09 20:00:05 +0800

    - add module

---
+++ Tool01/library.cpp
@@ -0,0 +1,7 @@
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "--- library:hello, Hello, World!" << std::endl;
+}
---
+++ Tool01/library.h
@@ -0,0 +1,6 @@
+#ifndef TOOL_LIBRARY_H
+#define TOOL_LIBRARY_H
+
+void hello();
+
+#endif //TOOL_LIBRARY_H
