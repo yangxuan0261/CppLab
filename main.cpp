5d86edadf77e1535df77e418ac9e03076519e336
wilker <364105996@qq.com>
2020-02-09 15:15:52 +0800

    - init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    CTest::print();
+    system("pause");
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by wilker on 2020/2/8.
+//
+
+#include <iostream>
+#include "test.h"
+
+void CTest::print() {
+    std::cout << "--- CTest::print" << std::endl;
+}
