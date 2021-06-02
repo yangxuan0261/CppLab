23fb33daf003aa805dc37f6f127ce0e5d8f054b9
yangxuan <364105996@qq.com>
2021-06-02 11:41:52 +0800

    'Robot-DESKTOP-4AS8H60'

--- AlgorithmTest/BinaryTreeBigestDist.cpp
+++ AlgorithmTest/BinaryTreeBigestDist.cpp
@@ -9,39 +9,39 @@ using namespace std;
 
 namespace BinaryTreeBigestDist {
 
-    int myindex = 0;  //全局索引变量
+    int myindex = 0;  // 全局索引变量
 
     typedef struct Node {
-        struct Node *pleft;     //左孩子
-        struct Node *pright;    //右孩子
-        char chValue;           //该节点的值
+        struct Node *pleft;     // 左孩子
+        struct Node *pright;    // 右孩子
+        char chValue;           // 该节点的值
 
-        int leftMaxValue;       //左子树最长距离
-        int rightMaxValue;      //右子树最长距离
+        int leftMaxValue;       // 左子树最长距离
+        int rightMaxValue;      // 右子树最长距离
     } BinTree;
 
     void findMaxLen(BinTree *root, int *maxLen) {
-        //遍历到叶子结点，返回
+        // 遍历到叶子结点，返回
         if (root == NULL)
             return;
 
-        //如果左子树为空，那么该节点左边最长距离为0
+        // 如果左子树为空，那么该节点左边最长距离为0
         if (root->pleft == NULL)
             root->leftMaxValue = 0;
 
-        //如果右子树为空，那么该节点右边最长距离为0
+        // 如果右子树为空，那么该节点右边最长距离为0
         if (root->pright == NULL)
             root->rightMaxValue = 0;
 
-        //如果左子树不为空，递归寻找左子树最长距离
+        // 递归处理子节点, 如果左子树不为空，递归寻找左子树最长距离
         if (root->pleft != NULL)
             findMaxLen(root->pleft, maxLen);
 
-        //如果右子树不为空，递归寻找右子树最长距离
+        // 递归处理子节点, 如果右子树不为空，递归寻找右子树最长距离
         if (root->pright != NULL)
             findMaxLen(root->pright, maxLen);
 
-        //计算左子树中距离根节点的最长距离
+        // 回溯是获取子节点, 计算左子树中距离根节点的最长距离
         if (root->pleft != NULL) {
             if (root->pleft->leftMaxValue > root->pleft->rightMaxValue)
                 root->leftMaxValue = root->pleft->leftMaxValue + 1;
@@ -49,7 +49,7 @@ namespace BinaryTreeBigestDist {
                 root->leftMaxValue = root->pleft->rightMaxValue + 1;
         }
 
-        //计算右子树中距离根节点的最长距离
+        // 回溯是获取子节点, 计算右子树中距离根节点的最长距离
         if (root->pright != NULL) {
             if (root->pright->leftMaxValue > root->pright->rightMaxValue)
                 root->rightMaxValue = root->pright->leftMaxValue + 1;
@@ -84,9 +84,9 @@ namespace BinaryTreeBigestDist {
     //创建二叉树
     void buildBinTreeByData(BinTree *&root, char data[]) {
         char ch = data[myindex++];
-        if (ch == '#')        //若输入的是空格符，表明二叉树为空，置*root为NULL
+        if (ch == '#')        // 若输入的是空格符，表明二叉树为空，置*root为NULL
             root = NULL;
-        else {               //若输入的不是空格符，则将该值赋值给根节点的chValue, 递归建立左子树和右子树
+        else {               // 若输入的不是空格符，则将该值赋值给根节点的chValue, 递归建立左子树和右子树
             root = (BinTree *) malloc(sizeof(BinTree));
             root->chValue = ch;
             root->leftMaxValue = 0;
@@ -97,7 +97,7 @@ namespace BinaryTreeBigestDist {
         }
     }
 
-    //销毁二叉树，释放内存
+    // 销毁二叉树，释放内存
     void destroyBinTree(BinTree *root) {
         if (root != NULL) {
             destroyBinTree(root->pleft);
@@ -108,7 +108,7 @@ namespace BinaryTreeBigestDist {
         }
     }
 
-    //前序遍历二叉树
+    // 前序遍历二叉树
     void preOrderTraverse(BinTree *root) {
         if (root != NULL) {
             printf("%c ", root->chValue); // 前序遍历
